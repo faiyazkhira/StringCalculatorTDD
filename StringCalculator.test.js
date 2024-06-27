8483e16ffa3add6c391974022a9fa10ac9bf5b18
faiyaz khira <faiyazkhira27@hmail.com>
2024-06-27 10:05:09 +0530

    add feature to handle negative numbers

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -33,3 +33,9 @@ test('should support different delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//|\n1|2|3")).toBe(6);
 });
+
+//TC007 - Handling Negative Numbers
+test('should throw an error for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+    expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2,-3");
+});
