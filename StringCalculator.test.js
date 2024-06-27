dc658a013f3ca2811d1e6ddfd5e2aa34f4ef0729
faiyaz khira <faiyazkhira27@hmail.com>
2024-06-27 08:58:30 +0530

    add feature to handle two numbers

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -10,3 +10,9 @@ test('should return the number itself for a single number string', () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
 });
+
+//TC003 - Handling Two Numbers
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("3,5")).toBe(8);
+});
