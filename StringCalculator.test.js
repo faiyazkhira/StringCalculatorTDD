cd9544aa3e8058d75db4458e7237ccccaae36bc3
faiyaz khira <faiyazkhira27@hmail.com>
2024-06-27 08:54:20 +0530

    add feature to handle single number

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,5 +1,12 @@
 const add = require('./StringCalculator');
 
+//TC001 - Handling Empty String
 test('should return 0 for an empty string', ()=>{
     expect(add('')).toBe(0);
 })
+
+//TC002 - Handling a Single Number
+test('should return the number itself for a single number string', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+});
